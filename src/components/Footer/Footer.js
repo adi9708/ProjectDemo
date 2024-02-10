826a0dde70a8f689d5b943c50e88624b6df926ec
adi9708 <riyapatni@Swapnils-MacBook-Pro-2.local>
2024-02-10 12:01:25 -0800

    UPDATE: Updated footer files

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
     <div>
       <a href="https://tabapay.com/privacy.html" className={styles.linkItem}>Terms of Service</a>
       <a href="https://tabapay.com/processing-services-privacy-policy.html" className={styles.linkItem}>Privacy Policy</a>
-      <a href="https://tabapay.com/contact.html" className={styles.linkItem}>Contact Us</a>
+      <a href="https://www.example.com" className={styles.linkItem}>Contact Us</a>
     </div>
     <div className={styles.copyRight}>
       &copy; {new Date().getFullYear()} Your Next Developer. All rights reserved.
